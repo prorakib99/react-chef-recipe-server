65d78d9bd3ffb920f56ba15ba36f519c1fbb94ff
Your Name <you@example.com>
2024-01-04 04:09:40 +0600

    Implement Chef data load via dynamic chef Id

--- index.js
+++ index.js
@@ -3,10 +3,22 @@ const app = express();
 const cors = require('cors')
 const port = 5000;
 
+const chefs = require('./data/chef.json')
+
 app.use(cors())
 
 app.get('/', (req, res) => {
     res.send('Chef and Recipe server is Running')
+})
+
+app.get('/chef', (req, res) => {
+    res.send(chefs);
+})
+
+app.get('/chef/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedItem = chefs.find(chef => chef.id === parseInt(id));
+    res.send(selectedItem)
 })
 
 app.listen(port, () => {
